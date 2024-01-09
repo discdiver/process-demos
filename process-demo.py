a984f4bd5e1e654340bb78f73203f8e747a174e6
Jeff Hale <jeffmshale@gmail.com>
2024-01-08 21:56:18 -0500

    fix name

--- process-demo.py
+++ process-demo.py
@@ -8,6 +8,6 @@ def print_hi():
 
 if __name__ == "__main__":
     print_hi.from_source(
-        source="https://github.com/discdiver/process-demo.git",
+        source="https://github.com/discdiver/process-demos.git",
         entrypoint="flow.py:print_hi",
     ).deploy(name="gh-process-deploy", work_pool_name="process1")
